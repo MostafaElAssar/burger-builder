7988ab1be3991a4d0106e08265e5c1c6ac82f275
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2020-04-12 00:48:21 +0200

    Make sure app won't break on other browsers

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import authReducer from './store/reducers/auth';
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'development'
+  (process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
